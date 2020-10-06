af355aeb365a417c8c464fe9eb0601cbee9b6739
Natanael <natanaelsilva.cc16@gmail.com>
2020-10-05 23:48:10 -0300

    first algorithm

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "theAlgorithms"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/Sort/BubbleSort.scala
@@ -0,0 +1,28 @@
+package Sort
+
+import scala.util.control.Breaks._
+
+object BubbleSort {
+
+  def bubbleSort(array: Array[Int]): Array[Int] = {
+    breakable {
+      for (i <- 0 to array.length - 1) {
+        var swap = false
+
+        for (j <- 0 to array.length - 2) {
+          if (array(j) > array(j + 1)) {
+            val temp = array(j)
+            array(j) = array(j + 1)
+            array(j + 1) = temp
+            swap = true
+          }
+        }
+
+        if (!swap) {
+          break
+        }
+      }
+    }
+    array
+  }
+}
